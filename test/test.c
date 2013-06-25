ad15fb4880ba150d3b08c19ff7e248cc9cd82c09
cyphar <cyphar@cyphar.com>
2013-06-26 01:10:39 +1000

    added merge, copy and duplicate functions

--- test/test.c
+++ test/test.c
@@ -40,7 +40,39 @@ int main(void) {
 	printf("%s -> %d\n", "fircats", *(int *) ohm_search(table, "fircats", strlen("fircats") + 1));
 	printf("%s -> %d\n", "man", *(int *) ohm_search(table, "man", strlen("man") + 1));
 
+	ohm_t *new = ohm_init(2, NULL);
+
+	a = 3;
+	b = 4;
+	c = 2;
+
+	ohm_insert(table, "dsd", strlen("dsd") + 1, &a, sizeof(3));
+	ohm_insert(table, "fircs", strlen("fircs") + 1, &b, sizeof(4));
+	ohm_insert(table, "man", strlen("man") + 1, &c, sizeof(2));
+
+	printf("merging new hashmap with old\n");
+	ohm_merge(table, new);
+
+	printf("retreiving values from hashmap size %d\n", table->size);
+	printf("%s -> %d\n", "lolcats", *(int *) ohm_search(table, "lolcats", strlen("lolcats") + 1));
+	printf("%s -> %d\n", "fircats", *(int *) ohm_search(table, "fircats", strlen("fircats") + 1));
+	printf("%s -> %d\n", "man", *(int *) ohm_search(table, "man", strlen("man") + 1));
+	printf("%s -> %d\n", "fircs", *(int *) ohm_search(table, "fircs", strlen("fircs") + 1));
+	printf("%s -> %d\n", "dsd", *(int *) ohm_search(table, "dsd", strlen("dsd") + 1));
+
+	printf("duplicating merged hashmap\n");
+	ohm_t *dup = ohm_dup(table);
+
+	printf("retreiving values from hashmap size %d\n", dup->size);
+	printf("%s -> %d\n", "lolcats", *(int *) ohm_search(dup, "lolcats", strlen("lolcats") + 1));
+	printf("%s -> %d\n", "fircats", *(int *) ohm_search(dup, "fircats", strlen("fircats") + 1));
+	printf("%s -> %d\n", "man", *(int *) ohm_search(dup, "man", strlen("man") + 1));
+	printf("%s -> %d\n", "fircs", *(int *) ohm_search(dup, "fircs", strlen("fircs") + 1));
+	printf("%s -> %d\n", "dsd", *(int *) ohm_search(dup, "dsd", strlen("dsd") + 1));
+
 	printf("freeing hashmap\n");
 	ohm_free(table);
+	ohm_free(new);
+	ohm_free(dup);
 	return 0;
 } /* main() */
